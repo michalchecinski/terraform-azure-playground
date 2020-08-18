37cdf4588f044eed95e588de4a66ca74d82a52cf
Michał Chęciński <michal.checinski@outlook.com>
2020-08-18 19:59:43 +0200

    rename rg file

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   version = "~>2.0"
   features {}
 }
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
         name = "rg-terraform-mch"
         location = "westeurope"
 }
